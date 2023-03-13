6495a253d072a94a5c6fc7012cf7d4ae701399b1
rustxjvmxgo <rustxjvmxgo@gmail.com>
2023-03-13 22:07:13 +0800

    Initial commit

---
+++ src/main/java/initiative/testingprimer/springboottesting/myprimerplayground/MyPrimerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package initialive.testingprimer.springboottesting.myprimerplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrimerPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPrimerPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/initiative/testingprimer/springboottesting/myprimerplayground/MyPrimerPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package initialive.testingprimer.springboottesting.myprimerplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPrimerPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
